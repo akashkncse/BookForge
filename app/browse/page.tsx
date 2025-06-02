8245d560f0077bbd0a78cc3090de16f89be91240
Akash K N <akashkncse@gmail.com>
2025-06-02 23:35:06 +0530

    yeahds

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,16 +1,15 @@
 import BookCard from '@/components/BookCard';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
+import { Book } from '@prisma/client';
 
 export default async function BookListPage() {
     const books = await prisma.book.findMany();
-
+    
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grids-cols-4 gap-4">
-            {books.map((book) => (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {books.map((book: Book) => (
                 <BookCard key={book.id} book={book} />
             ))}
         </div>
-    )
+    );
 }
